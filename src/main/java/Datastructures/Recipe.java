07ee026eb6e747295c8960dfdc619df5a4da9a0f
sbaer2306 <sebastian.baer98@gmail.com>
2021-12-15 20:36:11 +0100

    Rezept upload Bild Skalierung hinzugefügt

--- src/main/java/Datastructures/Recipe.java
+++ src/main/java/Datastructures/Recipe.java
@@ -2,8 +2,10 @@ package Datastructures;
 
 import javafx.embed.swing.SwingFXUtils;
 import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 
 import javax.imageio.ImageIO;
+import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
@@ -36,6 +38,33 @@ public class Recipe {
 
     public void setImageFile(File imageFile) {
         ImageFile = imageFile;
+        try {
+            this.image =new Image(ImageFile.toURI().toString());
+            double width=image.getWidth( );
+            double height=image.getHeight( );
+            if(width>=300.00){
+                double faktor= 300/width;
+                int newheight=(int)(faktor*height);
+                BufferedImage originalImage = ImageIO.read(ImageFile);//change path to where file is located
+                int type = originalImage.getType() == 0 ? BufferedImage.TYPE_INT_ARGB : originalImage.getType();
+                BufferedImage resizeImageJpg = resizeImage(originalImage, type,300, newheight);
+                File tmpFile = new File("temp.jpg");
+                ImageIO.write(resizeImageJpg, "jpg", tmpFile);
+                this.ImageFile = tmpFile;
+            }
+
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+    }
+    }
+
+    private static BufferedImage resizeImage(BufferedImage originalImage, int type, int IMG_WIDTH, int IMG_HEIGHT) {
+        BufferedImage resizedImage = new BufferedImage(IMG_WIDTH, IMG_HEIGHT, type);
+        Graphics2D g = resizedImage.createGraphics();
+        g.drawImage(originalImage, 0, 0, IMG_WIDTH, IMG_HEIGHT, null);
+        g.dispose();
+
+        return resizedImage;
     }
 
     @Override
