e31bbb097ad203abc9703a7f645245e1c02817b6
dweiss196 <david.weiss.96@gmx.de>
2021-12-15 16:08:35 +0100

    Rezept Rating funktioniert, falls sich der Nutzer einloggt, werden seine geliketen oder disliketen Rezepte angezeigt.

--- src/main/java/recipeView/RecipeViewController.java
+++ src/main/java/recipeView/RecipeViewController.java
@@ -4,25 +4,29 @@ import DBController.RatingDBController;
 import Datastructures.Recipe;
 import Datastructures.UserModel;
 import Session.UserSession;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
+import javafx.scene.control.Tooltip;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
 import org.controlsfx.control.Notifications;
 
 import java.sql.SQLException;
+import java.sql.SQLIntegrityConstraintViolationException;
 import java.util.Arrays;
 
 public class RecipeViewController {
 
     private Recipe recipe;
-    private RatingController ratingController;
+    // private RatingController ratingController;
     // private UserSession userSession;
     private UserModel userModel;
-
     private RatingDBController ratingDBController;
     
     @FXML
@@ -44,7 +48,7 @@ public class RecipeViewController {
     private Label dislikesNumber;
 
     public RecipeViewController() {
-        this.ratingController = new RatingController();
+        // this.ratingController = new RatingController(userModel);
         // this.userSession = new UserSession();
         this.userModel = new UserSession().getUserSession();
         this.ratingDBController = new RatingDBController();
@@ -55,21 +59,27 @@ public class RecipeViewController {
             if(userModel.getLikedRecipeIDs().contains(recipe.getId())) {
                 boolean revoked = ratingDBController.revokeLike(userModel, recipe);
                 if(revoked) {
-                    System.out.println("like was revoked..");
                     userModel.getLikedRecipeIDs().remove(recipe.getId());
                     thumbUpWhite.setVisible(true);
                     thumbUpBlack.setVisible(false);
-                    likesNumber.setText(recipe.getLikes());
+                    likesNumber.setText("" + (Integer.parseInt(recipe.getLikes()) -1));
                 }
             }
             else {
-                boolean liked = ratingDBController.insertNewLike(userModel, recipe);
-                userModel.getLikedRecipeIDs().add(recipe.getId());
-                if(liked) {
-                    System.out.println("Was liked..");
-                    thumbUpWhite.setVisible(false);
-                    thumbUpBlack.setVisible(true);
-                    likesNumber.setText("" + (Integer.parseInt(recipe.getLikes()) +1));
+                if(userModel.getDislikedRecipeIDs().contains(recipe.getId())) {
+                    Notifications notification = Notifications.create();
+                    notification.title("Oops, das ging wohl schief :(");
+                    notification.text("Setze dein dislike zurück um das Rezept zu liken.. :)");
+                    notification.show();
+                }
+                else {
+                    boolean liked = ratingDBController.insertNewLike(userModel, recipe);
+                    userModel.getLikedRecipeIDs().add(recipe.getId());
+                    if(liked) {
+                        thumbUpWhite.setVisible(false);
+                        thumbUpBlack.setVisible(true);
+                        likesNumber.setText("" + (Integer.parseInt(recipe.getLikes()) +1));
+                    }
                 }
             }
         }
@@ -86,19 +96,25 @@ public class RecipeViewController {
             if(userModel.getDislikedRecipeIDs().contains(recipe.getId())) {
                 boolean revoked = ratingDBController.revokeDislike(userModel, recipe);
                 if(revoked) {
-                    System.out.println("Dislike was revoked..");
                     userModel.getDislikedRecipeIDs().remove(recipe.getId());
                     thumbDownBlack.setVisible(false);
-                    dislikesNumber.setText(recipe.getDislikes());
+                    dislikesNumber.setText("" + (Integer.parseInt(recipe.getDislikes()) -1));
                 }
             }
             else {
-                boolean disliked= ratingDBController.insertNewDisLike(userModel, recipe);
-                userModel.getDislikedRecipeIDs().add(recipe.getId());
-                if(disliked) {
-                    System.out.println("Was disliked..");
-                    thumbDownBlack.setVisible(true);
-                    dislikesNumber.setText("" + (Integer.parseInt(recipe.getDislikes()) +1));
+                if(userModel.getLikedRecipeIDs().contains(recipe.getId())) {
+                    Notifications notification = Notifications.create();
+                    notification.title("Oops, das ging wohl schief :(");
+                    notification.text("Setze dein like zurück um das Rezept zu disliken.. :)");
+                    notification.show();
+                }
+                else {
+                    boolean disliked= ratingDBController.insertNewDisLike(userModel, recipe);
+                    userModel.getDislikedRecipeIDs().add(recipe.getId());
+                    if(disliked) {
+                        thumbDownBlack.setVisible(true);
+                        dislikesNumber.setText("" + (Integer.parseInt(recipe.getDislikes()) +1));
+                    }
                 }
             }
         }
@@ -114,12 +130,30 @@ public class RecipeViewController {
         return recipe;
     }
 
+    private void displayRating() {
+        if(userModel != null) {
+            if(userModel.getLikedRecipeIDs().contains(recipe.getId())) {
+                thumbUpBlack.setVisible(true);
+            }
+            else {
+                thumbUpWhite.setVisible(true);
+            }
+            if(userModel.getDislikedRecipeIDs().contains(recipe.getId())) {
+                thumbDownBlack.setVisible(true);
+            }
+            else {
+                thumbDownWhite.setVisible(true);
+            }
+        }
+    }
+
 
     public void setRecipe(Recipe recipe) {
         this.recipe = recipe;
     }
 
     public void showRecipe(){
+
         ImageView img = (ImageView) container.getChildren().get(0);
         img.setImage(recipe.getImage());
 
@@ -156,6 +190,8 @@ public class RecipeViewController {
 
         ListView ingredients = (ListView) container.getChildren().get(12);
         ingredients.getItems().add(recipe.getIngredients());
+
+        displayRating();
 
 
     }
