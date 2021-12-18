c933373cb48b16b12d5b875c56cf37d1048a446a
Sebastian Bär <sebmastr3@gmail.com>
2021-12-18 17:32:44 +0100

    Errorclerk hinzugefügt

--- src/main/java/homepage/Main.java
+++ src/main/java/homepage/Main.java
@@ -1,11 +1,15 @@
 package homepage;
 
+import Clerks.ErrorClerk;
 import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
 
 import java.sql.SQLException;
 
@@ -20,6 +24,10 @@ public class Main extends Application {
         primaryStage.setScene(scene);
         primaryStage.show();
         primaryStage.setMinWidth(1016);
+        primaryStage.setOnCloseRequest(windowEvent -> {
+            ErrorClerk.getInstance().closeTempStage();
+            Platform.exit();
+        });
     }
 
     public static void main(String[] args) throws SQLException {
