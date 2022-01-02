c90317b08080f84011544b4fb6ad4c55b57a3583
D3rDaniel <daniel.vogel1999@gmail.com>
2022-01-02 07:49:45 +0100

    neues App Logo und Icon hinzugefügt

--- src/main/java/homepage/Entry.java
+++ src/main/java/homepage/Entry.java
@@ -4,8 +4,11 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
+
+import java.util.ResourceBundle;
 
 public class Entry extends Application {
 
@@ -18,6 +21,7 @@ public class Entry extends Application {
         primaryStage.setScene(scene);
         primaryStage.show();
         primaryStage.setMinWidth(1016);
+        primaryStage.getIcons().add(new Image(String.valueOf(getClass().getResource("/appIcon/Food Hub icon.png"))));
     }
 
     public static void launchApplication(String[] args) {
