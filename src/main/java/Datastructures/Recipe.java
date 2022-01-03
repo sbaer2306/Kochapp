010a8c79fce7b838e800faa203ed304011f4e46f
Sebastian Bär <sebmastr3@gmail.com>
2022-01-03 13:43:43 +0100

    unnötiger auskommentierter Code gelöscht

--- src/main/java/Datastructures/Recipe.java
+++ src/main/java/Datastructures/Recipe.java
@@ -37,39 +37,11 @@ public class Recipe {
     }
 
     public void setImageFile(File imageFile) throws InterruptedException {
-        /*ImageFile = imageFile;
-        try {
-            this.image =new Image(ImageFile.toURI().toString());
-            double width=image.getWidth( );
-            double height=image.getHeight( );
-            if(width>=300.00){
-                double faktor= 300/width;
-                int newheight=(int)(faktor*height);
-                BufferedImage originalImage = ImageIO.read(ImageFile);//change path to where file is located
-                int type = originalImage.getType() == 0 ? BufferedImage.TYPE_INT_ARGB : originalImage.getType();
-                BufferedImage resizeImageJpg = resizeImage(originalImage, type,300, newheight);
-                File tmpFile = new File("temp.jpg");
-                ImageIO.write(resizeImageJpg, "jpg", tmpFile);
-                this.ImageFile = tmpFile;
-            }
-
-        } catch (IOException e) {
-            System.out.println(e.getMessage());
-        }*/
         ImageResizeClerk imageResizeClerk = new ImageResizeClerk();
         imageResizeClerk.start();
         this.ImageFile=imageResizeClerk.resizeImage(imageFile);
         imageResizeClerk.join();
     }
-
-    /*private static BufferedImage resizeImage(BufferedImage originalImage, int type, int IMG_WIDTH, int IMG_HEIGHT) {
-        BufferedImage resizedImage = new BufferedImage(IMG_WIDTH, IMG_HEIGHT, type);
-        Graphics2D g = resizedImage.createGraphics();
-        g.drawImage(originalImage, 0, 0, IMG_WIDTH, IMG_HEIGHT, null);
-        g.dispose();
-
-        return resizedImage;
-    }*/
 
     @Override
     public String toString() {
