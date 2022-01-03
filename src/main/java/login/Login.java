e1d829bf056ad79b200dfcfb7ed07ab498b67586
D3rDaniel <daniel.vogel1999@gmail.com>
2022-01-03 15:28:28 +0100

    If anweisung für bestehenden Login im Login controller gelöscht --> Überflüssig

--- src/main/java/login/Login.java
+++ src/main/java/login/Login.java
@@ -13,13 +13,7 @@ public class Login {
 
     public Login() throws SQLException {
 
-        if(new UserSession().sessionExists()){
-            Notifications note = Notifications.create();
-            note.title("Login Fehlgeschlagen");
-            note.text("Sie sind bereits angemeldet als "  + new UserSession().getUserSession().getUsername() + "!");
-            note.show();
-        }
-        else{
+
             Stage loginInterface = new Stage();
             FXMLLoader loader = new FXMLLoader(getClass().getResource("login.fxml"));
             loginInterface.setTitle("Login");
@@ -32,6 +26,5 @@ public class Login {
             loginInterface.showAndWait();
             LoginController lc = loader.getController();
             lc.login();
-        }
     }
 }
